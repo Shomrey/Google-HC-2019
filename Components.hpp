025855f17c9649086f6452a45913118055c6dc23
KaKaDaka <44410552+medycynka@users.noreply.github.com>
2019-02-28 20:21:37 +0100

    Add files via upload

--- Components.hpp
+++ Components.hpp
@@ -3,30 +3,32 @@
 #include <iostream>
 #include <string>
 #include <vector>
+
 class Photo{
-    public:
+public:
 
     Photo(int id, char orient, int no_of_tags, std::vector<std::string> tags): photo_id(id), orientation(orient), no_of_tags(no_of_tags), tags(tags){};
     int get_id()    {return photo_id;};
     char get_orientation() {return orientation;};
     void show_photo() {std::cout<<"id: "<<photo_id<<", no_of_tags: "<<no_of_tags<<std::endl<<"tags: "; for(auto tag : tags) std::cout<<tag<<", ";std::cout<<std::endl;};
-    private:
-    
+    int get_num_of_tags(){ return no_of_tags;}
+    std::vector<std::string> get_tags(){ return tags;}
+private:
+
     int photo_id;
     std::vector<std::string> tags;
     int no_of_tags;
     char orientation;
 
-    
+
 };
 
 class Slide {
-    public:
+public:
     void add_photo(Photo photo) {photos.push_back(photo);};
-    vector<Photo> get_photos() {return photos;};
-    private:
+    std::vector<Photo> get_photos() {return photos;};
+private:
     std::vector<Photo> photos;
 };
 
 #endif//COMPONENTS_H
-
