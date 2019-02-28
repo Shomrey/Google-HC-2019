5ad5699dd2ef08f5feef79caa0cceae45e5e62bb
Piotr Michta <38496988+GaleWombat@users.noreply.github.com>
2019-02-28 19:28:25 +0100

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -3,26 +3,43 @@
 #include <string>
 
 using namespace std;
-class zdjecie{
-
+class Zdjecie{
+    public:
+    Zdjecie(char orient, int no_of_tags);
+    int pobierz_id()    {return id_zdj;}
+    private:
     int id_zdj;
     vector<string> tags;
     int ilosc_tagow;
     char orientacja;
 
-    int pobierz_id()
-    {
-        return id_zdj;
+    
+};
+
+Zdjecie::Zdjecie(char orient, int no_of_tags): orientacja(orient), ilosc_tagow(no_of_tags){}
+
+class Slajd {
+    public:
+    Slajd();
+    private:
+    vector<Zdjecie> zdjecia;
+
+
+};
+int main(int argc, char* argv[]) {
+    vector<Zdjecie> wczytane_zdjecia;
+    int ilosc_zdjec = 0, it = 0, no_of_tags = 0;
+    char orient;
+    vector<string> tagi;
+    string tag;
+    cin >> ilosc_zdjec;
+    while(it<ilosc_zdjec){
+        tagi.clear();
+        cin >> orient;
+        cin >> no_of_tags;
+        Zdjecie zdj(orient,no_of_tags);
+        
+        it++;
     }
-};
-
-class slajd {
-
-    vector<zdjecie> zdjecia;
-
-
-};
-int main() {
-    std::cout << "Hello, World!" <<f()<< std::endl;
     return 0;
 }
