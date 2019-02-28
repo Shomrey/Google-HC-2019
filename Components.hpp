fa04fef3fb86d0ad15d23244bebea94d9c9aa994
Piotr Michta <38496988+GaleWombat@users.noreply.github.com>
2019-02-28 21:29:10 +0100

    Add files via upload

--- Components.hpp
+++ Components.hpp
@@ -3,12 +3,13 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 class Photo{
 public:
 
     Photo(int id, char orient, int no_of_tags, std::vector<std::string> tags): photo_id(id), orientation(orient), no_of_tags(no_of_tags), tags(tags){};
-    Photo(Photo& source); //cc-tor
+    Photo(const Photo& source); //cc-tor
     int get_id()    {return photo_id;};
     char get_orientation() {return orientation;};
     void show_photo() {std::cout<<"id: "<<photo_id<<", no_of_tags: "<<no_of_tags<<std::endl<<"tags: "; for(auto tag : tags) std::cout<<tag<<", ";std::cout<<std::endl;};
@@ -23,7 +24,7 @@ private:
 
 
 };
-Photo::Photo(Photo& source){
+Photo::Photo(const Photo& source){
     photo_id = source.photo_id;
     orientation = source.orientation;
     no_of_tags = source.no_of_tags;
@@ -32,10 +33,26 @@ Photo::Photo(Photo& source){
 
 class Slide {
 public:
-    void add_photo(Photo photo) {photos.push_back(photo);};
+    void add_photo(Photo photo);
     std::vector<Photo> get_photos() {return photos;};
 private:
     std::vector<Photo> photos;
+    std::vector<string> tags_of_photos;
+};
+
+void Slide::add_photo(Photo photo){
+    photos.push_back(photo);
+    if(tags_of_photos.empty()){
+        for(auto tag : photo.get_tags()) tags_of_photos.push_back(tag);
+    }
+    else{
+        for(auto tag : photo.get_tags()) if(std::find(tags_of_photos.begin(),tags_of_photos.end(),tag)==tags_of_photos.end()) tags_of_photos.push_back(tag);
+    }
+}
+
+class Graf{
+public:
+    std::vector<std::vector<Photo>> graphik;
 };
 
 #endif//COMPONENTS_H
